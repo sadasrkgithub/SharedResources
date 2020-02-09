df447663acc525412dc8c9be845c80d45c3cd9ab
sadasrkgithub <sadasiva.k@tectoro.com>
2020-02-09 15:01:49 +0530

    Project Push

---
+++ SharedProject/src/com/sample1/First/Demo.java
@@ -0,0 +1,17 @@
+package com.sample1.First;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\64bitAutomationSW\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+	}
+
+}
